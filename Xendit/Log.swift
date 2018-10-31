3d8ed9cb2349300c9ab0ccf5cf454843aba07d8e
Vladimir Lyukov <v.lyukov@gmail.com>
2018-10-31 13:11:00 +0300

    Log failed network requests to LogDNA

--- Xendit/Log.swift
+++ Xendit/Log.swift
@@ -16,6 +16,10 @@ internal class Log {
 
     var isEnabled = true
 
+    init() {
+        ISHLogDNAService.setup(withIngestionKey: "db91ce0574a2ef343bd753485b81b0bd", hostName: "xendit.co", appName: "iOS SDK")
+    }
+
     func log(_ level: Level = .info, _ message: String) {
         print("[xendit] \(message)")
     }
@@ -24,13 +28,33 @@ internal class Log {
         log(.verbose, message)
     }
 
-    func logUrlResponse(prefix: String, data: Data?, response: URLResponse?, error: Error?) {
+    func logUrlResponse(prefix: String, request: URLRequest, data: Data?, response: URLResponse?, error: Error?) {
         verbose("\(prefix) finished request")
-        if let response = response {
-            verbose("response: \(response)")
-        }
+        let dataString: String?
         if let data = data {
-            verbose("data: \(String(data: data, encoding: .utf8) ?? "non-string")")
+            dataString = String(data: data, encoding: .utf8)
+        } else {
+            dataString = nil
+        }
+        if let response = response as? HTTPURLResponse {
+            verbose("response: \(response)")
+            if response.statusCode >= 400 {
+                let level = response.statusCode >= 500 ? ISHLogDNALevel.error : ISHLogDNALevel.warn
+                let message = ISHLogDNAMessage(
+                    line: "Failed request: \(request.httpMethod ?? "n/a") \(request.url?.absoluteString ?? "n/a")",
+                    level: level,
+                    meta: [
+                        "statusCode" : response.statusCode,
+                        "requestURL": request.url?.absoluteString ?? "",
+                        "responseHeaders": (response.allHeaderFields as? [String: String]) ?? [:],
+                        "responseBody": dataString ?? ""
+                    ]
+                )
+                ISHLogDNAService.logMessages([message]);
+            }
+        }
+        if let dataString = dataString {
+            verbose("data: \(dataString)")
         }
         if let error = error {
             verbose("error: \(error)")
