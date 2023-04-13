97e14e937a52c16aff008c7929076c9cc21876c3
andysg012 <andy@xendit.co>
2023-04-13 15:47:09 +0800

    add Sentry dependency

--- Package.swift
+++ Package.swift
@@ -19,13 +19,17 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.1.0"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.3.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Xendit",
-            dependencies: ["XenditObjC"],
+            dependencies: [
+                "XenditObjC",
+                .product(name: "Sentry", package: "sentry-cocoa"),
+            ],
             exclude: ["Info.plist"]),
         .target(
             name: "XenditObjC",
