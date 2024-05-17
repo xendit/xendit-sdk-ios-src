523d213aec127519a243808c77328ec09d1573c0
Duong Dao <duong.dao@xendit.co>
2024-05-17 16:27:27 +0700

    Fixed integration conflict with Sentry

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.1.0"),
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.8.0"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", exact: "6.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
