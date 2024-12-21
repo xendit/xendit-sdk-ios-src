25cbabb81144d8945e5bf8c6c6f49974e4057ee4
ahmadAlfhajri <82349749+ahmadAlfhajri@users.noreply.github.com>
2024-12-21 23:24:40 +0800

    TEST from fork for SDK

--- Tests/XenditTests/Helpers/TestCards.swift
+++ Tests/XenditTests/Helpers/TestCards.swift
@@ -11,4 +11,6 @@ import Foundation
 enum TestCard {
     static let validCardNo3ds = "5200000000000056"
     static let validCardWith3ds = "4000000000000002"
+    static let validCard = "4000000000001091"
+    static let invalidCard = "4000000000001099"
 }
